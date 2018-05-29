e7f5f22ee51cd9139e38587b6826a4253afc8a83
mn15104 <mn15104@my.bristol.ac.uk>
2018-05-29 12:41:50 +0100

    fixed sdl draw functions

--- app/main.hs
+++ app/main.hs
@@ -3,44 +3,75 @@
 module Main 
     where
 
-import Data.Vec as Vec hiding (map)
-import GLM as GLM
-import SDL_Aux as SDLAux
-import qualified SDL as SDL 
-import Control.Monad (forM, unless, void)
+import Prelude hiding (any, mapM_)
+import Control.Monad hiding (mapM_)
+import Data.Foldable hiding (elem)
+import Data.Maybe
 import Foreign.C.Types
+import SDL.Vect
+import SDL (($=))
+import qualified SDL
+import qualified GLM as GLM
+import SDL_Aux
 
-testdraw :: Screen -> CInt -> CInt -> Bool -> IO Screen
-testdraw screen i j isWhite
-  | j >= (height screen) = return screen
-  | i >= (width screen) = testdraw screen 0 (j + 1)
-  | isWhite = do
-      success <- SDL.rendererDrawColor (renderer screen) 255 255 255 1 
-      SDL.renderDrawPoint (renderer screen) i j
-      testdraw screen (i+1) j False
-  | not isWhite = do
-      success <- SDL.rendererDrawColor (renderer screen) 0 0 0 1 
-      SDL.renderDrawPoint (renderer screen) i j
-      testdraw screen (i+1) j True
+
+
 
 loop :: Screen -> SDL.Texture -> SDL.Window -> IO ()
 loop screen text window  = do
   poll <- SDL.pollEvents
   -- If elapsed time > 100, update game
   let quit = elem SDL.QuitEvent $ map SDL.eventPayload poll
-  screen' <- testdraw screen 0 0 True
-  sdl_renderframe screen'
+--   screen' <- testdraw screen 0 0 True
+--   sdl_renderframe screen'
 --   SDL.present (renderer screen)
-  unless quit $ loop screen' text window
+  unless quit $ loop screen text window
 
+
+
+draw :: Screen -> Int -> IO ()
+draw (Screen window renderer targetTexture screenWidth screenHeight screenCenter) theta = do             
+
+
+    SDL.rendererDrawColor renderer $= V4 maxBound maxBound maxBound maxBound
+    SDL.clear renderer
+
+    SDL.rendererDrawColor renderer $= V4 maxBound 0 0 maxBound
+    SDL.fillRect renderer (Just $ SDL.Rectangle (P $ V2 (screenWidth `div` 4) (screenHeight `div` 4))
+                                                        (V2 (screenWidth `div` 2) (screenHeight `div` 2)))
+
+    SDL.rendererDrawColor renderer $= V4 0 0 maxBound maxBound
+    SDL.drawRect renderer (Just (SDL.Rectangle (P $ V2 (screenWidth `div` 6) (screenHeight `div` 6))
+                                                    (V2 (screenWidth * 2 `div` 3) (screenHeight * 2 `div` 3))))
+
+    SDL.rendererDrawColor renderer $= V4 0 maxBound 0 maxBound
+    SDL.drawLine renderer (P (V2 0 (screenHeight `div` 2))) (P (V2 screenWidth (screenHeight `div` 2)))
+
+    SDL.rendererDrawColor renderer $= V4 maxBound maxBound maxBound maxBound
+    for_ [0, 4 .. screenHeight] $ \i ->
+        SDL.drawPoint renderer (P (V2 (screenWidth `div` 2) i))
+
+   
+    return ()    
 
 main :: IO ()
 main = do
     -- window & surface & renderer
     screen <- sdl_init
+    let screenCenter = P (V2 ((width screen) `div` 2) ((height screen) `div` 2))
+        loop theta = do
+            events <- map SDL.eventPayload <$> SDL.pollEvents
+            let quit = SDL.QuitEvent `elem` events
+            sdl_set_render_target (renderer screen) (Just $ texture screen)
 
-    loop screen (texture screen) (window screen)
+            draw screen theta
 
-    SDL.destroyWindow $ window screen
+            sdl_set_render_target (renderer screen) Nothing
+            sdl_render_texture (renderer screen) (texture screen) 0 Nothing (Just (fromIntegral theta)) (Just $ center screen) Nothing
+            SDL.present (renderer screen)
+            unless quit (loop (theta + 2 `mod` 360))
+    
+    loop (0 :: Int)
+
+    SDL.destroyWindow (window screen)
     SDL.quit
-        
--- app/sdl_aux.hs
+++ app/sdl_aux.hs
@@ -1,35 +1,18 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module SDL_Aux
-    where
+module SDL_Aux where
 
-import Control.Concurrent (threadDelay)
+
+import Prelude hiding (any, mapM_)
+import Control.Monad hiding (mapM_)
+import Data.Foldable hiding (elem)
+import Data.Maybe
 import Foreign.C.Types
-import qualified Data.Word8 as Word
-import Data.Vec as Vec hiding (map)
-import Data.StateVar
-import Control.Applicative
-import Control.Exception
-import Control.Monad (forM, unless, void)
-import Control.Monad.IO.Class (MonadIO, liftIO)
-import Control.Monad.Trans
-import GLM as GLM
-import qualified Data.Bits as Bit
-import Data.Data (Data)
-import Data.Foldable
-import qualified Data.ByteString as Byte
-import Data.Maybe (isJust, fromMaybe)
-import Data.Monoid (First(..))
-import Data.Text (Text)
-import Data.Typeable
-import Debug.Trace
-import SDL.Vect hiding (column)
-import qualified SDL as SDL
-import System.FilePath.Posix
-import System.Random
-import Control.Monad.State as ST
-import qualified SDL.Raw.Timer as Raw
-import qualified SDL.Raw.Types as Raw
+import SDL.Vect
+import SDL (($=))
+import qualified SDL
 
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (800, 800)
@@ -39,63 +22,66 @@ data Texture = Texture SDL.Texture (V2 CInt)
 -- # Screen
 data Screen = Screen  { window    :: SDL.Window,
                         renderer  :: SDL.Renderer,
-                        surface   :: SDL.Surface,
-                        texture   :: SDL.Texture,
-                        buffer    :: Byte.ByteString,
+                        texture   :: Texture,
                         height    :: CInt,
-                        width     :: CInt
+                        width     :: CInt,
+                        center    :: Point V2 CInt
                       }
 
-sdl_renderframe :: Screen -> IO ()
-sdl_renderframe t_screen = do
-                        t <- SDL.updateTexture (texture t_screen) Nothing (buffer t_screen) ((width t_screen))
-                        let screen' = t_screen {texture = t}
-                        cl <- SDL.clear (renderer screen')
-                        co <- SDL.copy (renderer screen') (texture screen') Nothing Nothing
-                        pres <- SDL.present (renderer screen')
-                        return pres
+
+sdl_create_blank :: SDL.Renderer -> V2 CInt -> SDL.TextureAccess -> IO Texture
+sdl_create_blank r sz access = Texture <$> SDL.createTexture r SDL.RGBA8888 access sz <*> pure sz
+
+sdl_render_texture :: SDL.Renderer -> Texture -> Point V2 CInt -> Maybe (SDL.Rectangle CInt) -> Maybe CDouble -> Maybe (Point V2 CInt) -> Maybe (V2 Bool) -> IO ()
+sdl_render_texture r (Texture t size) xy clip theta center flips =
+  let dstSize =
+        maybe size (\(SDL.Rectangle _ size') -> size') clip
+  in SDL.copyEx r
+                t
+                clip
+                (Just (SDL.Rectangle xy dstSize))
+                (fromMaybe 0 theta)
+                center
+                (fromMaybe (pure False) flips)
+
+sdl_set_render_target :: SDL.Renderer -> Maybe Texture -> IO ()
+sdl_set_render_target r Nothing = SDL.rendererRenderTarget r $= Nothing
+sdl_set_render_target r (Just (Texture t _)) = SDL.rendererRenderTarget r $= Just t
+
+sdl_init :: IO Screen
+sdl_init = do
+  SDL.initialize [SDL.InitVideo]
+
+  SDL.HintRenderScaleQuality $= SDL.ScaleLinear
+  do  renderQuality <- SDL.get SDL.HintRenderScaleQuality
+      when (renderQuality /= SDL.ScaleLinear) $
+        putStrLn "Warning: Linear texture filtering not enabled!"
+
+  window <-
+    SDL.createWindow
+      "SDL Tutorial"
+      SDL.defaultWindow {SDL.windowInitialSize = V2 screenWidth screenHeight}
+  SDL.showWindow window
+
+  renderer <-
+    SDL.createRenderer
+      window
+      (-1)
+      SDL.RendererConfig
+        { SDL.rendererType = SDL.AcceleratedVSyncRenderer
+        , SDL.rendererTargetTexture = False
+        }
+
+  SDL.rendererDrawColor renderer $= V4 maxBound maxBound maxBound maxBound
+
+  targetTexture <- sdl_create_blank renderer (V2 screenWidth screenHeight) SDL.TextureAccessTarget
+  let screenCenter = P (V2 (screenWidth `div` 2) (screenHeight `div` 2))
+  return $ Screen window renderer targetTexture screenHeight screenWidth screenCenter
+
 
 sdl_noquit :: IO () -> IO ()
 sdl_noquit next = do
-                  poll <- SDL.pollEvents
-                  let quit = elem SDL.QuitEvent $ map SDL.eventPayload poll
-                  return $ unless quit $ next
-                  return ()
-
-sdl_init :: IO (Screen)
-sdl_init = do
-      SDL.initialize [SDL.InitVideo]
-      SDL.HintRenderScaleQuality $= SDL.ScaleLinear
-      -- Create window
-      window  <- SDL.createWindow "My Window" SDL.defaultWindow { SDL.windowInitialSize = V2 screenWidth screenHeight }
-      SDL.showWindow window
-      -- Create surface
-      surface <- SDL.getWindowSurface window
-      -- Fill window
-      let white =  V4 maxBound maxBound maxBound maxBound
-      SDL.surfaceFillRect surface Nothing white
-      renderer <- SDL.createRenderer window (-1) SDL.RendererConfig {
-        SDL.rendererType = SDL.AcceleratedRenderer,
-        SDL.rendererTargetTexture = False
-      }
-      texture <- SDL.createTexture renderer SDL.ARGB8888 SDL.TextureAccessStatic (V2 screenWidth screenHeight)
-      SDL.rendererDrawColor renderer $= V4 maxBound maxBound maxBound maxBound
-      let buffer = Byte.pack $ [0 | j <- [0 .. screenHeight*screenWidth]]
-      let screen = Screen window renderer surface texture buffer screenHeight screenWidth
-      return screen
-
-sdl_put_pixel :: Screen -> CInt -> CInt -> Vec3D -> IO Screen
-sdl_put_pixel screen x y color = do
-                            let success = (x < 0 || x >= width screen || y < 0 || y >= height screen)
-                            case success of
-                              True -> return screen
-                              False -> do
-                                    let r = GLM.clamp (255*((Vec.toList color) !! 0)) 0 255
-                                    let g = GLM.clamp (255*((Vec.toList color) !! 1)) 0 255
-                                    let b = GLM.clamp (255*((Vec.toList color) !! 2)) 0 255
-                                    let uscreen = Byte.unpack ( buffer screen)
-                                    let (xs,ys) = splitAt (fromIntegral $ toInteger (y*(width screen) + x)) uscreen
-                                    let insert =  ( (Bit.shift 24 128)) Bit..|.  (Bit.shift (toInteger $ floor r) 16) Bit..|.  (Bit.shift (toInteger $ floor g) 8) Bit..|. (toInteger $ floor b)
-                                    let newscreen = screen {buffer = Byte.pack (xs ++ ( fromInteger insert: ys))}
-                                    return newscreen
--- sdl_setcolors :: 
+                poll <- SDL.pollEvents
+                let quit = elem SDL.QuitEvent $ map SDL.eventPayload poll
+                return $ unless quit $ next
+                return ()
