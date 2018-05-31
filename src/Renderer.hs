1dc5725a2411a38a726b8901f29e30b334ac1850
mn15104 <mn15104@my.bristol.ac.uk>
2018-06-01 00:31:15 +0100

    fill triangle function finished with z-buffer added

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -8,6 +8,7 @@ import Control.Monad hiding (mapM_)
 import Data.Foldable hiding (elem)
 import Data.Maybe
 import Data.Word8
+import Data.List
 import Data.Cross
 import Foreign.C.Types
 import SDL.Vect
@@ -16,6 +17,7 @@ import qualified SDL
 import qualified GLM as GLM
 import SDL_Aux
 import Scene
+import Control.Lens
 
 -- # Triangle Vertices v0 v1 v2 -> Queried Point p -> Barycentric Coordinates
 barycentric :: (V2 Double, V2 Double, V2 Double) -> V2 Double -> V3 Double
@@ -26,27 +28,46 @@ barycentric (v0, v1, v2) p = if abs b2 < 1 then (V3 (-1) 1 1) else V3 (1 - (b0 +
                               V2 v1x v1y = v1 
                               V2 v2x v2y = v2
 
-draw_triangle :: Screen ->  (V2 Double, V2 Double, V2 Double) -> Triangle -> IO ()
-draw_triangle screen projected_vertices triangle = do
+replaceAt :: Double -> Int -> [Double] ->[Double]
+replaceAt newElement n array = take n array ++ [newElement] ++ drop (n + 1) array
+
+
+-- # Screen -> Projected 2D Triangle Vertices v0 v1 v2 -> Z-Buffer  -> Triangle  -> Updated Z-Buffer                     
+draw_triangle :: Screen ->  (V3 Double, V3 Double, V3 Double) -> [Double] -> Triangle -> IO [Double]
+draw_triangle screen projected_vertices zbuffer triangle  = do
     let bound_min = V2 ((fromIntegral $ toInteger $ width screen) - (1.0 :: Double) ) (((fromIntegral $ toInteger $ height screen) - 1.0 ))
         bound_max = V2 (0 :: Double) (0 :: Double)
         clamped = bound_min
-        (v0, v1, v2) = projected_vertices
-        bbox_min = V2 bbox_min_x bbox_min_y
-        bbox_max = V2 bbox_max_x bbox_max_y
-        bbox_min_x =  max 0 (foldr (\(V2 x y) (b) -> (min x  b) ) ((\(V2 xb yb) -> min xb yb) bound_min) [v0, v1, v2])
-        bbox_min_y =  max 0 (foldr (\(V2 x y) (b) -> (min y  b) ) ((\(V2 xb yb) -> min xb yb) bound_min) [v0, v1, v2])
-        bbox_max_x =  min ((\(V2 xb yb) -> xb) clamped) (foldr (\(V2 x y) (b) -> (max y b) ) ((\(V2 xb yb) -> max xb yb) bound_max) [v0, v1, v2])
-        bbox_max_y =  min ((\(V2 xb yb) -> yb) clamped) (foldr (\(V2 x y) (b) -> (max y b) ) ((\(V2 xb yb) -> max xb yb) bound_max) [v0, v1, v2])
-    fillpoints <- sequence $ [ sdl_put_pixel screen ( V2 (fromInteger (floor px)) (fromInteger (floor py)) ) (color triangle) | px <- [bbox_min_x .. bbox_max_x], py <- [bbox_min_y .. bbox_max_y], 
-                                        let (V3 barx bary barz) = barycentric (projected_vertices) (V2 (realToFrac  px) (realToFrac py)), 
-                                                barx >= 0 && bary >=0 && barz >=0 ]
-   
-    return ()
-  
+
+        (V3 v0x v0y v0z, V3 v1x v1y v1z, V3 v2x v2y v2z) = projected_vertices
+        projected_vert_2D = (V2 v0x v0y, V2 v1x v1y, V2 v2x v2y)
+
+        bbox_min_x =  max 0 (foldr (\(V2 x y) (b) -> (min x  b) ) ((\(V2 xb yb) -> min xb yb) bound_min)  (concat  $ map (^..each) [projected_vert_2D])    )
+        bbox_min_y =  max 0 (foldr (\(V2 x y) (b) -> (min y  b) ) ((\(V2 xb yb) -> min xb yb) bound_min) (concat  $ map (^..each) [projected_vert_2D]) )
+        bbox_max_x =  min ((\(V2 xb yb) -> xb) clamped) (foldr (\(V2 x y) (b) -> (max y b) ) ((\(V2 xb yb) -> max xb yb) bound_max) (concat  $ map (^..each) [projected_vert_2D]) )
+        bbox_max_y =  min ((\(V2 xb yb) -> yb) clamped) (foldr (\(V2 x y) (b) -> (max y b) ) ((\(V2 xb yb) -> max xb yb) bound_max) (concat  $ map (^..each) [projected_vert_2D]) )
+        fillpoints =  sequence $ [ [px, py, zdepth, fromIntegral zbuffer_index]
+                                    | px <- [bbox_min_x .. bbox_max_x], py <- [bbox_min_y .. bbox_max_y], 
+                                                let (V3 barx bary barz) = barycentric (V2 v0x v0y, V2 v1x v1y, V2 v2x v2y) (V2 (realToFrac  px) (realToFrac py)),
+                                                let zdepth = sum $ zipWith (*)  [v0z, v1z, v2z] [barx, bary, barz],
+                                                let zbuffer_index = (floor px + (floor py) * (fromIntegral $ toInteger $ width screen)),
+                                                    barx >= 0 && bary >=0 && barz >=0 && (zbuffer !! zbuffer_index) < zdepth]
+    sequence $ map (\([px, py, zdepth, zbuffer_index]) -> sdl_put_pixel screen ( V2 (fromInteger px) (fromInteger py) ) (color triangle)) (map (map floor)  fillpoints)
     
+    let f (zbuffer') fillpoints' = case fillpoints' of
+                                    (x:xs) -> let zbuffer'' = (\([px, py, zdepth, zbuffer_index]) -> replaceAt zdepth (floor zbuffer_index) (zbuffer') ) x
+                                              in f (zbuffer'' :: [Double]) xs
+                                    []      -> (zbuffer'  :: [Double])
+        zbuffer' = f zbuffer fillpoints
+    
+    return zbuffer'
+  
+-- draw_triangle     
 
 -- draw_triangle :: Screen -> V3 Int -> Triangle -> IO ()
 -- draw_triangle screen origin triangle = do
 --     let (V4 v1a v1b v1c v1d, V4 v2a v2b v2c v2d, V4 v3a v3b v3c v3d) = points triangle
 -- sdl_draw_line screen v0 v1 (color triangle)
+-- sdl_put_pixel screen ( V2 (fromInteger (floor px)) (fromInteger (floor py)) ) (color triangle)
+
+      -- k =  map (\[px, py, zdepth, zbuffer_index] -> sdl_put_pixel screen ( V2 px py ) (color triangle)) 
