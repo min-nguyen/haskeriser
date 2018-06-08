0627ef5b633ea2cbd414f749302e1f9c4276d7d3
mn15104 <mn15104@my.bristol.ac.uk>
2018-06-08 08:42:14 +0100

    draw model face semi-complete

--- src/Renderer.hs
+++ src/Renderer.hs
@@ -58,54 +58,113 @@ draw_loop screen model light camera = do
                                         
                                         process_triangles (idx + 1) ) (screen_world_coords !! idx)
 
-    process_triangles 0
-    --     -- For Each Face Do This
-    -- mapM (\(screen, world) -> do
-    --         let (world_0, world_1, world_2) = world
-    --             norm = norm_V3 $ or_V3  (world_2 - world_0) (world_1 - world_2)
-    --             light_intensity = norm * (direction light)
-    --         return ()) screen_world_coords
-    --         if light_intensity > 0 then let uv = model_uv model  
+    process_triangles 0 
     return ()
 
--- # Screen -> Projected 2D Triangle Vertices v0 v1 v2 -> Z-Buffer  -> Triangle  -> Updated Z-Buffer                     
-draw_triangle :: Screen ->  (V4 Double, V4 Double, V4 Double) -> [Double] -> Triangle -> IO [Double]
-draw_triangle screen projected_vertices zbuffer triangle  = do
-    let bound_min = V2 ((fromIntegral $ toInteger $ width screen) - (1.0 :: Double) ) (((fromIntegral $ toInteger $ height screen) - 1.0 ))
-        bound_max = V2 (0 :: Double) (0 :: Double)
-        clamped = bound_min
+-- #             Screen ->   Projected 2D Triangle Vertices   ->   UV Coordinates Z-Buffer  -> Updated Z-Buffer                     
+draw_triangle :: Screen ->  (V3 Int, V3 Int, V3 Int) ->  (V2 Int, V2 Int, V2 Int) -> [Double] -> IO [Double]
+draw_triangle screen screen_vertices uv_vertices zbuffer  = do
+    let ((V3 v0x v0y v0z, V3 v1x v1y v1z, V3 v2x v2y v2z), (V2 v0u v0v, V2 v1u v1v, V2 v2u v2v)) = order_vertices_i screen_vertices uv_vertices
+        ((v0, v1, v2), (uv0, uv1, uv2)) = ((V3 v0x v0y v0z, V3 v1x v1y v1z, V3 v2x v2y v2z), (V2 v0u v0v, V2 v1u v1v, V2 v2u v2v))
+        triangle_height = v2y - v0y
 
-        (V4 v0x v0y v0z v0w, V4 v1x v1y v1z v1w, V4 v2x v2y v2z v2w) = projected_vertices
-        projected_vert_2D = (V2 v0x v0y, V2 v1x v1y, V2 v2x v2y)
+    [((V3 vAx vAy vAz, V3 vBx vBy vBz), (V2 vAu vAv, V2 vBu vBv))] <- mapM (\i -> do
+            let h = fromIntegral i
 
-        bbox_min_x =  max 0 (foldr (\(V2 x y) (b) -> (min x  b) ) ((\(V2 xb yb) -> min xb yb) bound_min)  (concat  $ map (^..each) [projected_vert_2D])    )
-        bbox_min_y =  max 0 (foldr (\(V2 x y) (b) -> (min y  b) ) ((\(V2 xb yb) -> min xb yb) bound_min) (concat  $ map (^..each) [projected_vert_2D]) )
-        bbox_max_x =  min ((\(V2 xb yb) -> xb) (fromIntegral $ toInteger $ width screen)) (foldr (\(V2 x y) (b) -> (max y b) ) ((\(V2 xb yb) -> max xb yb) bound_max) (concat  $ map (^..each) [projected_vert_2D]) )
-        bbox_max_y =  min ((\(V2 xb yb) -> yb) (fromIntegral $ toInteger $ height screen)) (foldr (\(V2 x y) (b) -> (max y b) ) ((\(V2 xb yb) -> max xb yb) bound_max) (concat  $ map (^..each) [projected_vert_2D]) )
+                second_half = (h > v1y - v0y) || (v1y == v0z) 
+                segment_height = if second_half then v2y - v1y else v1y - v0y
+                alpha = h `div` (fromIntegral triangle_height)
+                beta = if second_half then (h - (v1y - v0y)) `div` segment_height else h `div` segment_height
+
+                vA = v0 + (mul_V3_Num (v2 - v0) ( alpha))  
+                vB = if second_half then v1 + (mul_V3_Num (v2 - v1) beta) else v0 + (mul_V3_Num (v1 - v0) beta)
+                uvA = uv0 + (mul_V2_Num (uv2 - uv0) ( alpha))   
+                uvB = if second_half then uv1 + (mul_V2_Num (uv2 - uv1) beta) else uv0 + (mul_V2_Num (uv1 - uv0) beta)
+
+                -- (vAi, vBi) = mapTuple2 $ mapTuple3 floor (vA, vB)
+                -- (uvAi, uvBi) = mapTuple2 $ mapTuple2 floor (uvA, uvB)
+            return $ order_min_x_i (vA, vB) (uvA, uvB)) ([0 .. (triangle_height)] :: [Int])
+
+    let ((vA', vB'), (uvA', uvB')) = ((V3 vAx vAy vAz, V3 vBx vBy vBz), (V2 vAu vAv, V2 vBu vBv))
+
+    
+    return []
+
+draw_helper :: ((V3 Int, V3 Int), (V2 Int, V2 Int)) -> Screen -> Int -> [Double] -> [Double]
+draw_helper ((V3 vAx vAy vAz, V3 vBx vBy vBz), (V2 vAu vAv, V2 vBu vBv)) screen index zbuffer = 
+                    let ((vA', vB'), (uvA', uvB')) =  ((V3 vAx vAy vAz, V3 vBx vBy vBz), (V2 vAu vAv, V2 vBu vBv)) 
+                        phi = if vBx == vAx then (1.0 :: Double) else ((((fromIntegral index) - (fromIntegral vAx)) / ((fromIntegral vBx) - (fromIntegral vAx))))
+                        (V3 px py pz) = vA' + (mul_V3_Num (vB' - vA') (floor phi))
+                        (V2 pu pv) = uvA' + (mul_V2_Num (uvB' - uvA') (floor phi))
+                        idx = px + py * (fromIntegral $ width_i screen)
+                    in  (if (zbuffer !! (idx)) < (fromIntegral pz)
+                        then (replaceAt (fromIntegral pz) idx zbuffer)  
+                        else zbuffer)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    --( ([(floor vAx) .. (floor vBx)]) :: [Int]  )
+    --     bound_min = V2 ((fromIntegral $ toInteger $ width screen) - (1.0 :: Double) ) (((fromIntegral $ toInteger $ height screen) - 1.0 ))
+    --     bound_max = V2 (0 :: Double) (0 :: Double)
+    --     clamped = bound_min
+
+    --     (V4 v0x v0y v0z v0w, V4 v1x v1y v1z v1w, V4 v2x v2y v2z v2w) = projected_vertices
+    --     projected_vert_2D = (V2 v0x v0y, V2 v1x v1y, V2 v2x v2y)
+
+    --     bbox_min_x =  max 0 (foldr (\(V2 x y) (b) -> (min x  b) ) ((\(V2 xb yb) -> min xb yb) bound_min)  (concat  $ map (^..each) [projected_vert_2D])    )
+    --     bbox_min_y =  max 0 (foldr (\(V2 x y) (b) -> (min y  b) ) ((\(V2 xb yb) -> min xb yb) bound_min) (concat  $ map (^..each) [projected_vert_2D]) )
+    --     bbox_max_x =  min ((\(V2 xb yb) -> xb) (fromIntegral $ toInteger $ width screen)) (foldr (\(V2 x y) (b) -> (max y b) ) ((\(V2 xb yb) -> max xb yb) bound_max) (concat  $ map (^..each) [projected_vert_2D]) )
+    --     bbox_max_y =  min ((\(V2 xb yb) -> yb) (fromIntegral $ toInteger $ height screen)) (foldr (\(V2 x y) (b) -> (max y b) ) ((\(V2 xb yb) -> max xb yb) bound_max) (concat  $ map (^..each) [projected_vert_2D]) )
         
-        fillpoints =   [ (px, py, zdepth, fromIntegral zbuffer_index)
-                                    | px <- [bbox_min_x .. bbox_max_x], py <- [bbox_min_y .. bbox_max_y], 
-                                                let (V3 barx bary barz) = barycentric (V2 v0x v0y, V2 v1x v1y, V2 v2x v2y) (V2 (realToFrac  px) (realToFrac py)),
-                                                let zdepth = sum $ zipWith (*)  [v0z, v1z, v2z] [barx, bary, barz],
-                                                let zbuffer_index = (floor px + (floor py) * (fromIntegral $ toInteger $ width screen)),
-                                                barx >= 0 && bary >=0 && barz >=0 && (zbuffer !! zbuffer_index) < zdepth]
-    print (fillpoints)
-    sequence $ map (\(px,py,zdepth,zbuffer_index) -> 
-                        sdl_put_pixel screen ( V2 (fromInteger px) (fromInteger py) ) (color triangle)) 
-                            (map (\(px', py', zdepth', zind') -> (floor px', floor py',floor zdepth',floor zind'))  fillpoints)
+    --     fillpoints =   [ (px, py, zdepth, fromIntegral zbuffer_index)
+    --                                 | px <- [bbox_min_x .. bbox_max_x], py <- [bbox_min_y .. bbox_max_y], 
+    --                                             let (V3 barx bary barz) = barycentric (V2 v0x v0y, V2 v1x v1y, V2 v2x v2y) (V2 (realToFrac  px) (realToFrac py)),
+    --                                             let zdepth = sum $ zipWith (*)  [v0z, v1z, v2z] [barx, bary, barz],
+    --                                             let zbuffer_index = (floor px + (floor py) * (fromIntegral $ toInteger $ width screen)),
+    --                                             barx >= 0 && bary >=0 && barz >=0 && (zbuffer !! zbuffer_index) < zdepth]
+    -- print (fillpoints)
+    -- sequence $ map (\(px,py,zdepth,zbuffer_index) -> 
+    --                     sdl_put_pixel screen ( V2 (fromInteger px) (fromInteger py) ) (color triangle)) 
+    --                         (map (\(px', py', zdepth', zind') -> (floor px', floor py',floor zdepth',floor zind'))  fillpoints)
     
-    let f (zbuffer') fillpoints' = case fillpoints' of
-                                    (x:xs) -> let zbuffer'' = (\(px, py, zdepth, zbuffer_index) -> replaceAt zdepth (floor zbuffer_index) (zbuffer') ) x
-                                              in f (zbuffer'' :: [Double]) xs
-                                    []      -> (zbuffer'  :: [Double])
-        zbuffer' = f zbuffer fillpoints
+    -- let f (zbuffer') fillpoints' = case fillpoints' of
+    --                                 (x:xs) -> let zbuffer'' = (\(px, py, zdepth, zbuffer_index) -> replaceAt zdepth (floor zbuffer_index) (zbuffer') ) x
+    --                                           in f (zbuffer'' :: [Double]) xs
+    --                                 []      -> (zbuffer'  :: [Double])
+    --     zbuffer' = f zbuffer fillpoints
     
-    return zbuffer'
+    --'
 
 
-    
 
+order_min_x :: (V3 Double, V3 Double) -> (V2 Double, V2 Double) -> ((V3 Double, V3 Double), (V2 Double, V2 Double))
+order_min_x (V3 vAx vAy vAz, V3 vBx vBy vBz) (V2 vAu vAv, V2 vBu vBv)
+    | (vAx > vBx) = ((V3 vBx vBy vBz, V3 vAx vAy vAz), (V2 vBu vBv, V2 vAu vAv))
+    | otherwise   = ((V3 vAx vAy vAz, V3 vBx vBy vBz), (V2 vAu vAv, V2 vBu vBv))
 
+order_vertices :: (V3 Double, V3 Double, V3 Double) -> (V2 Double, V2 Double, V2 Double) -> ((V3 Double, V3 Double, V3 Double), (V2 Double, V2 Double, V2 Double))
+order_vertices (V3 v0x v0y v0z, V3 v1x v1y v1z, V3 v2x v2y v2z)  (V2 v0u v0v, V2 v1u v1v, V2 v2u v2v)
+    | (v0y > v1y) = order_vertices (V3 v1x v1y v1z, V3 v0x v0y v0z, V3 v2x v2y v2z)  (V2 v1u v1v, V2 v0u v0v, V2 v2u v2v)
+    | (v0y > v2y) = order_vertices (V3 v2x v2y v2z, V3 v1x v1y v1z, V3 v0x v0y v0z)  (V2 v2u v2v, V2 v1u v1v, V2 v0u v0v)
+    | (v1y > v2y) = order_vertices (V3 v0x v0y v0z, V3 v2x v2y v2z, V3 v1x v1y v1z)  (V2 v0u v0v, V2 v2u v2v, V2 v1u v1v) 
+    | otherwise   = ((V3 v0x v0y v0z, V3 v1x v1y v1z, V3 v2x v2y v2z), (V2 v0u v0v, V2 v1u v1v, V2 v2u v2v))
+
+order_min_x_i :: (V3 Int, V3 Int) -> (V2 Int, V2 Int) -> ((V3 Int, V3 Int), (V2 Int, V2 Int))
+order_min_x_i (V3 vAx vAy vAz, V3 vBx vBy vBz) (V2 vAu vAv, V2 vBu vBv)
+    | (vAx > vBx) = ((V3 vBx vBy vBz, V3 vAx vAy vAz), (V2 vBu vBv, V2 vAu vAv))
+    | otherwise   = ((V3 vAx vAy vAz, V3 vBx vBy vBz), (V2 vAu vAv, V2 vBu vBv))
+
+order_vertices_i :: (V3 Int, V3 Int, V3 Int) -> (V2 Int, V2 Int, V2 Int) -> ((V3 Int, V3 Int, V3 Int), (V2 Int, V2 Int, V2 Int))
+order_vertices_i (V3 v0x v0y v0z, V3 v1x v1y v1z, V3 v2x v2y v2z)  (V2 v0u v0v, V2 v1u v1v, V2 v2u v2v)
+    | (v0y > v1y) = order_vertices_i (V3 v1x v1y v1z, V3 v0x v0y v0z, V3 v2x v2y v2z)  (V2 v1u v1v, V2 v0u v0v, V2 v2u v2v)
+    | (v0y > v2y) = order_vertices_i (V3 v2x v2y v2z, V3 v1x v1y v1z, V3 v0x v0y v0z)  (V2 v2u v2v, V2 v1u v1v, V2 v0u v0v)
+    | (v1y > v2y) = order_vertices_i (V3 v0x v0y v0z, V3 v2x v2y v2z, V3 v1x v1y v1z)  (V2 v0u v0v, V2 v2u v2v, V2 v1u v1v) 
+    | otherwise   = ((V3 v0x v0y v0z, V3 v1x v1y v1z, V3 v2x v2y v2z), (V2 v0u v0v, V2 v1u v1v, V2 v2u v2v))
 
 -- cam_mat = cam_projection_matrix camera
 -- viewport_mat = viewport_matrix (fromIntegral $ toInteger $ width screen)/8.0 (fromIntegral $ toInteger $ height screen)/8.0 (fromIntegral $ toInteger $ width screen)*0.75 (fromIntegral $ toInteger $ height screen)*0.75
