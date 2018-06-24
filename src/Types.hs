0736732ee76acdf3283af6202cfb823d90f649e3
mn15104 <mn15104@my.bristol.ac.uk>
2018-06-24 16:04:20 +0100

    shader monad draft added

--- src/Types.hs
+++ src/Types.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BangPatterns #-}
-
+{-# LANGUAGE InstanceSigs #-}
 module Types
     where
 
@@ -17,51 +17,60 @@ import Foreign.C.Types
 import Data.Word8
 import Data.Binary.Get
 import Data.Word
+import Control.Applicative 
+import Control.Monad (liftM, ap)
+import Control.Monad.State
 import qualified Data.ByteString as B
 import Data.Vector.Storable as ST
 import Codec.Picture
 import Codec.Picture.Types
 
-data Model = Model {verts       :: V.Vector (Vec3 Double, Int),
-                    faces       :: V.Vector ([(Vec3 Integer, Int)]),
-                    norms       :: V.Vector (Vec3 Double, Int),
-                    uvs         :: V.Vector (Vec2 Double, Int),
-                    diffuse_map :: TGA_Header,
-                    nfaces :: Int,
-                    nverts :: Int}
+---- |‾| ---------------------------------------------------------------------------- |‾| ----
+---- | |                       Data type & Instance Definitions                       | | ----
+---- |_| ---------------------------------------------------------------------------- |_| ----
 
-data Rasteriser = Rasteriser {  getModel       :: Model,
+data Model = Model {    
+                        verts       :: V.Vector (Vec3 Double, Int),
+                        faces       :: V.Vector ([(Vec3 Integer, Int)]),
+                        norms       :: V.Vector (Vec3 Double, Int),
+                        uvs         :: V.Vector (Vec2 Double, Int),
+                        diffuse_map :: TGA_Header,
+                        nfaces :: Int,
+                        nverts :: Int
+                    }
+
+data Rasteriser = Rasteriser {  
+                                getModel       :: Model,
                                 getScreen      :: Screen,
                                 getCamera      :: Camera,
-                                getLight       :: Light}
-
-data Shader     = Shader    {   modelview       :: Vec4 (Vec4 Double),
-                                viewport        :: Vec4 (Vec4 Double),
-                                projection      :: Vec4 (Vec4 Double),
-                                uniform_M       :: Vec4 (Vec4 Double),
-                                uniform_MIT     :: Vec4 (Vec4 Double),
-                                uniform_Mshadow :: Vec4 (Vec4 Double),
-                                varying_uv      :: Vec2 (Vec3 Double),
-                                varying_tri     :: Vec3 (Vec3 Double) }
-
-data TGA_Header = TGA_Header {  width   :: {-# UNPACK #-} !Int,
-								height  :: {-# UNPACK #-} !Int,
-								imgdata :: ST.Vector (PixelBaseComponent PixelRGB8),
-								bbp :: Int,
-								img    :: Image PixelRGB8
+                                getLight       :: Light
                             }
-				 | TGA_Error
 
+newtype Kernel s a = Kernel  {  
+                                runKernel       ::   s -> (a, s)
+                             }
+
+data Shader  =       Shader { 
+                                modelview       ::  Vec4 (Vec4 Double),
+                                viewport        ::  Vec4 (Vec4 Double),
+                                projection      ::  Vec4 (Vec4 Double),
+                                uniform_M       ::  Vec4 (Vec4 Double),
+                                uniform_MIT     ::  Vec4 (Vec4 Double),
+                                uniform_Mshadow ::  Vec4 (Vec4 Double),
+                                varying_uv      ::  Vec2 (Vec3 Double),
+                                varying_tri     ::  Vec3 (Vec3 Double)
+                               
+                            }            
 
 data Light = Light {direction   :: Vec3 Double}
 
 data Camera = Camera {  position :: Vec4 Double }
 
--- # SDL.Texture, (Width, Height)
+                            -- (Width, Height) --
 data Texture = Texture SDL.Texture (V2 CInt)
--- # Screen
 
-data Screen = Screen  { window    :: SDL.Window,
+data Screen = Screen  { 
+                        window    :: SDL.Window,
                         renderer  :: SDL.Renderer,
                         texture   :: Texture,
                         height_c  :: {-# UNPACK #-} !CInt,
@@ -71,8 +80,20 @@ data Screen = Screen  { window    :: SDL.Window,
                       }
 
 
-data Triangle = Triangle {  points  :: (Vec4 Double, Vec4 Double, Vec4 Double),
-                            color   :: Vec4 Word8 }
+
+data TGA_Header = TGA_Header {  
+                                width   :: {-# UNPACK #-} !Int,
+								height  :: {-# UNPACK #-} !Int,
+								imgdata :: ST.Vector (PixelBaseComponent PixelRGB8),
+								bbp     :: Int,
+								img     :: Image PixelRGB8
+                             }
+				 | TGA_Error
+
+data Triangle = Triangle {  
+                            points  :: (Vec4 Double, Vec4 Double, Vec4 Double),
+                            color   :: Vec4 Word8 
+                         }
 
 data Color = Red | Blue | Yellow | Green | White | Purple
 
@@ -82,3 +103,27 @@ type ScreenCoords = Vec3 (Vec3 Int)
 
 screenWidth, screenHeight :: CInt
 (screenWidth, screenHeight) = (800, 800)
+
+
+
+get  :: Kernel s s
+get  = Kernel {runKernel = (\s -> (s, s)) }
+
+set  :: s -> Kernel s ()
+set s = Kernel {runKernel = (\s' -> ((), s) ) }
+
+instance Monad (Kernel s) where
+    (>>=) :: Kernel s a -> (a -> Kernel s b) -> Kernel s b
+    f >>=  g = Kernel {runKernel = (\s -> (let (a', s')      = (runKernel f) s
+                                           in (runKernel (g a')) s'))
+                                          }
+    return :: a -> Kernel s a
+    return x = Kernel  {runKernel = (\shader_st -> (x, shader_st))}
+
+
+instance Functor (Kernel s) where 
+    fmap = liftM
+
+instance Applicative (Kernel s) where
+    pure = return
+    (<*>) = ap
